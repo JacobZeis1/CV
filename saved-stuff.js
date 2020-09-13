2a76d8e4788ae9db35e9f18bc609465b22c862a0
Jacob Zeis <jacobzeis@sbcglobal.net>
2020-09-13 01:03:30 -0700

    yo

---
+++ saved-stuff.js
@@ -0,0 +1,51 @@
+<div class="big-contain">
+    <div class="card-container">
+           <a href="index.html">Home</a>
+    <div class="card">
+        <div class="card--display"><i class="material-icons">Team Project</i>
+          <h2>Fashion Website</h2>
+          <!-- <img class="craftin" src="./img/ligne.png" alt="" width="370" height="210"> -->
+        </div>
+        <div class="card--hover">
+          <h2>My Ligne</h2>
+          <p>A team of 3 others and I had two weeks to fully plan and build this webapp. With only a few weeks of prior JS and full stack development experience, I was able to play a crucial role in my team and build core features for our website. This includes a fully functional globe-map on the homepage.
+          </p>
+          <p><strong>Tech used:</strong> Ruby, HTML, CSS, JavaScript | Sass, Bootstrap, jQuery</p>
+          <p class="link">
+          <li>
+            <a href="http://www.my-ligne.com/" target="_blank"><i class="fas fa-globe"></i>Website</a></li>
+            <li><a class="btn btn-ghost" href="https://github.com/bingwanli/ligne" target="_blank"><i class="fab fa-github"></i>Github repo</a></li></p>
+        </div>
+      <div class="card--border"></div>
+    </div>
+
+    <div class="card">
+        <div class="card--display"><i class="material-icons">Accessibility plugin for</i>
+          <h1 class='blind'>blind gamers</h1>
+          <!-- <img class="craftin" src="./img/craft.png"> -->
+        </div>
+        <div class="card--hover">
+          <h2>Background:</h2>
+          <p>A childhood friend of mine in his recent years was diagnosed with a rare disease that impaired most of his vision. He is still working out what he can and cannot do, but he continues to surprise everyone around him with what he can accomplish. That is until we tried playing Minecraft together. He struggled to read what was in his inventory and craft items he had previously known. Minecraft's native accessibility features did not aid his problems, which was suprising and made me mad. I decided to learn java and within a few hours I had made a plugin that read out every item he clicked on. His diamonds continue to stack up.</p>
+          <p><strong>Tech used:</strong> Java</p>
+          <p class="link"><li>
+            <a href="https://youtu.be/_m6VbLo4fTU" target="_blank"><i class="fab fa-youtube"></i>Youtube video demo</a></li>
+          </p>
+        </div>
+      <div class="card--border"></div>
+    </div>
+
+      <div class="card">
+        <div class="card--display"><i class="material-icons">Airbnb-like interactive map</i>
+          <h1 class='blind'>mini project with react</h1>
+          <!-- <img class="craftin" src="./img/craft.png"> -->
+        </div>
+        <div class="card--hover">
+          <h2>Background:</h2>
+          <p></p>
+          <p><strong>Tech used:</strong> Java</p>
+          <p class="link"><li>
+            <a href="" target="_blank"><i class="fas fa-globe"></i>Demo site</a></li>
+          </p>
+        </div>
+      <div class="card--border"></div>
